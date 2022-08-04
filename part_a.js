fe313a30cba82ccc89ed93ca26a6bd43847e4b91
Softwareguruguru <happyotienoh@gmail.com>
2022-08-04 10:48:15 +0300

    created js note files

---
+++ part_a.js
@@ -0,0 +1,43 @@
+var a;
+
+a = 7;
+
+console.log(a) //displays valu a at the console(terminal)
+
+var difference = 10-5; suubtrsaction
+var product = 7*7; multiplication
+var quotient = 77/7; division
+
+//incrementing a number
+var myvar = 10;
+myvar++; /*or */ myvar = myvar+1;
+
+
+//derementing a number
+var myvar = 10;
+myvar--; /*or */ myvar = myvar-1;
+
+//remainder of  a number
+var remainder;
+remainder = 20 % 3; //modulus
+
+//compound augmented addition of a number
+var a = 7;
+a = a + 3; //or
+a +=3; //augmenting
+
+//compound augmented subtraction of a number
+var a = 7;
+a = a - 3; //or
+a -=3; //augmenting
+
+//compound augmented multiplication of a number
+var a = 7;
+a = a * 3; //or
+a *=3; //augmenting
+
+//compound augmented division of a number
+var a = 7;
+a = a / 3; //or
+a /=3; //augmenting
+
---
+++ part_b.js
@@ -0,0 +1,25 @@
+/*  
+single quote = \'
+double quote = \"
+backslash = \\
+new line = \n
+carriage return = \r
+tab = \t
+backspace = \b
+form feed = \f
+concatenating = +
+
+*/
+
+//concatenating = +
+
+var first = "Happy";
+var second = first + " Blackie ";
+console.log(second); 
+
+// or
+var first = "Happy"
+first += "Blackie"
+console.log(first); 
+
+//Appending variables and strings.
